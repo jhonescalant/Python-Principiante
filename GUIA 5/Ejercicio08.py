48a820d9aa1734d47bc48dd70bc59c2bdb2fb230
Jhon Escalante <112142953+jhonescalant@users.noreply.github.com>
2022-11-25 12:28:31 -0300

    Update Ejercicio08.py

--- GUIA 5/Ejercicio08.py
+++ GUIA 5/Ejercicio08.py
@@ -1,7 +1,16 @@
 # Desarrollar una rutina tal que dados una base y un exponente, enteros positivos, calcule  y retorne la potencia.
 
+# Inicio del programa
+def potencia(base, exponente):
+    resultado=0
+    if (base < 0 or exponente < 0):
+        print( "Numero fuera de rango")
+    
+    resultado= base ** exponente
+    return resultado
 
-
+print(potencia(50, -5))
+print(potencia(10,2))
 
 
 

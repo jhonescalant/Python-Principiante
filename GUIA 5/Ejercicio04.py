47f6de0fae21f91fb3d024eeaa224c748892506b
Jhon Escalante <112142953+jhonescalant@users.noreply.github.com>
2022-11-25 12:10:03 -0300

    Update Ejercicio04.py

--- GUIA 5/Ejercicio04.py
+++ GUIA 5/Ejercicio04.py
@@ -4,21 +4,28 @@
 # c) Determinar la cantidad de palabras con la cantidad de letras pares y comienzan con la letra “c”
 # d) Determinar el porcentaje que representa el primer punto sobre el total de las palabras del texto procesado.
 
+#Inicio del programa
+def simplificaciones(P, Q):
+    numeros_primos = (2, 3, 5, 7, 11)
 
+    for n in numeros_primos:  
+        if (P % n == 0 and Q % n == 0):
+            P /= n
+            Q /= n
 
+    return [int(P), int(Q)]
 
+#Casos de prueba
+primera_fraccion = simplificaciones(105, 210)
+segunda_fraccion = simplificaciones(15, 45)
+tercera_fraccion = simplificaciones(22, 44)
+cuarta_fraccion  = simplificaciones(9, 24)
 
-
-
-
-
-
-
-
-
-
-
-
+# Salida por pantalla 
+print(f"{primera_fraccion[0]}/{primera_fraccion[1]}")
+print(f"{segunda_fraccion [0]}/{segunda_fraccion [1]}")
+print(f"{tercera_fraccion[0]}/{tercera_fraccion[1]}")
+print(f"{cuarta_fraccion[0]}/{cuarta_fraccion[1]}")
 
 
 
